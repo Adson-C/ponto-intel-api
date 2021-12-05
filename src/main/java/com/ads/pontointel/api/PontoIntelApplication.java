2f396a700744e1fef76fbc780c6f69cf28cb4be5
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-12-04 21:43:00 -0300

    Commit Inicial do projeto

---
+++ src/main/java/com/ads/pontointel/api/PontoIntelApplication.java
@@ -0,0 +1,13 @@
+package com.ads.pontointel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoIntelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoIntelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ads/pontointel/api/PontoIntelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ads.pontointel.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoIntelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
